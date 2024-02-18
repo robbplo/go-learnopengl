98a781a5916f88686b62f9f65a53d3377e43e30f
Robbin Ploeger <robbin.ploeger@bettyblocks.com>
2024-02-18 18:53:24 +0100

    spinny cube

--- main.go
+++ main.go
@@ -49,10 +49,43 @@ func main() {
 	gl.GenVertexArrays(1, &vao)
 	gl.BindVertexArray(vao)
 
-	vertices := []float32{
-		0.0, 1.0, 0.0,
-		1.0, -1.0, 0.0,
-		-1.0, -1.0, 0.0,
+	cube_vertices := []float32{
+		-1.0, -1.0, -1.0, // triangle 1 : begin
+		-1.0, -1.0, 1.0,
+		-1.0, 1.0, 1.0, // triangle 1 : end
+		1.0, 1.0, -1.0, // triangle 2 : begin
+		-1.0, -1.0, -1.0,
+		-1.0, 1.0, -1.0, // triangle 2 : end
+		1.0, -1.0, 1.0,
+		-1.0, -1.0, -1.0,
+		1.0, -1.0, -1.0,
+		1.0, 1.0, -1.0,
+		1.0, -1.0, -1.0,
+		-1.0, -1.0, -1.0,
+		-1.0, -1.0, -1.0,
+		-1.0, 1.0, 1.0,
+		-1.0, 1.0, -1.0,
+		1.0, -1.0, 1.0,
+		-1.0, -1.0, 1.0,
+		-1.0, -1.0, -1.0,
+		-1.0, 1.0, 1.0,
+		-1.0, -1.0, 1.0,
+		1.0, -1.0, 1.0,
+		1.0, 1.0, 1.0,
+		1.0, -1.0, -1.0,
+		1.0, 1.0, -1.0,
+		1.0, -1.0, -1.0,
+		1.0, 1.0, 1.0,
+		1.0, -1.0, 1.0,
+		1.0, 1.0, 1.0,
+		1.0, 1.0, -1.0,
+		-1.0, 1.0, -1.0,
+		1.0, 1.0, 1.0,
+		-1.0, 1.0, -1.0,
+		-1.0, 1.0, 1.0,
+		1.0, 1.0, 1.0,
+		-1.0, 1.0, 1.0,
+		1.0, -1.0, 1.0,
 	}
 
 	// identify the vertex buffer
@@ -62,7 +95,51 @@ func main() {
 	// the following commands will talk about our 'vertexbuffer' buffer
 	gl.BindBuffer(gl.ARRAY_BUFFER, vertexbuffer)
 	// give our vertices to OpenGL
-	gl.BufferData(gl.ARRAY_BUFFER, len(vertices)*4, gl.Ptr(vertices), gl.STATIC_DRAW)
+	gl.BufferData(gl.ARRAY_BUFFER, len(cube_vertices)*4, gl.Ptr(cube_vertices), gl.STATIC_DRAW)
+
+	cube_colors := []float32{
+		0.583, 0.771, 0.014,
+		0.609, 0.115, 0.436,
+		0.327, 0.483, 0.844,
+		0.822, 0.569, 0.201,
+		0.435, 0.602, 0.223,
+		0.310, 0.747, 0.185,
+		0.597, 0.770, 0.761,
+		0.559, 0.436, 0.730,
+		0.359, 0.583, 0.152,
+		0.483, 0.596, 0.789,
+		0.559, 0.861, 0.639,
+		0.195, 0.548, 0.859,
+		0.014, 0.184, 0.576,
+		0.771, 0.328, 0.970,
+		0.406, 0.615, 0.116,
+		0.676, 0.977, 0.133,
+		0.971, 0.572, 0.833,
+		0.140, 0.616, 0.489,
+		0.997, 0.513, 0.064,
+		0.945, 0.719, 0.592,
+		0.543, 0.021, 0.978,
+		0.279, 0.317, 0.505,
+		0.167, 0.620, 0.077,
+		0.347, 0.857, 0.137,
+		0.055, 0.953, 0.042,
+		0.714, 0.505, 0.345,
+		0.783, 0.290, 0.734,
+		0.722, 0.645, 0.174,
+		0.302, 0.455, 0.848,
+		0.225, 0.587, 0.040,
+		0.517, 0.713, 0.338,
+		0.053, 0.959, 0.120,
+		0.393, 0.621, 0.362,
+		0.673, 0.211, 0.457,
+		0.820, 0.883, 0.371,
+		0.982, 0.099, 0.879,
+	}
+	// color buffer
+	var colorbuffer uint32
+	gl.GenBuffers(1, &colorbuffer)
+	gl.BindBuffer(gl.ARRAY_BUFFER, colorbuffer)
+	gl.BufferData(gl.ARRAY_BUFFER, len(cube_colors)*4, gl.Ptr(cube_colors), gl.STATIC_DRAW)
 
 	// Load shaders
 	programId, err := loadShaders("shaders/shader_vert.glsl", "shaders/shader_frag.glsl")
@@ -75,35 +152,44 @@ func main() {
 	projection := mgl32.Perspective(mgl32.DegToRad(80.0), float32(windowWidth)/float32(windowHeight), 0.1, 100.0)
 
 	// Camera matrix
-	view := mgl32.LookAtV(mgl32.Vec3{4, 3, 3}, mgl32.Vec3{0, 0, 0}, mgl32.Vec3{0, 1, 0})
+	view := mgl32.LookAtV(mgl32.Vec3{4, 3, -3}, mgl32.Vec3{0, 0, 0}, mgl32.Vec3{0, 1, 0})
 
 	// Model matrix
-  scale := mgl32.Scale3D(1.0, 2.0, 1.0)
-  rotate := mgl32.HomogRotate3D(mgl32.DegToRad(45.0), mgl32.Vec3{0, 0, 1})
-  translate := mgl32.Translate3D(0.0, 0.0, 0.0)
+	scale := mgl32.Scale3D(2.0, 2.0, 2.0)
+	rotate := mgl32.HomogRotate3D(mgl32.DegToRad(0.0), mgl32.Vec3{0, 0, 1})
+	translate := mgl32.Translate3D(0.0, 0.0, 0.0)
 	model := translate.Mul4(rotate).Mul4(scale)
 
-
-	mvp := projection.Mul4(view).Mul4(model)
-	mvpId := gl.GetUniformLocation(programId, gl.Str("MVP\x00"))
-
 	gl.ClearColor(0.0, 0.0, 0.4, 0.0)
+	// prevent rendering the back side of the cube
+	gl.Enable(gl.DEPTH_TEST)
+	gl.DepthFunc(gl.LESS)
 
 	glfw.GetCurrentContext().SetInputMode(glfw.StickyKeysMode, glfw.True)
+	// Main loop
 	for !window.ShouldClose() {
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 		gl.UseProgram(programId)
 
+		gl.UniformMatrix4fv(gl.GetUniformLocation(programId, gl.Str("model\x00")), 1, false, &model[0])
+    gl.UniformMatrix4fv(gl.GetUniformLocation(programId, gl.Str("view\x00")), 1, false, &view[0])
+    gl.UniformMatrix4fv(gl.GetUniformLocation(programId, gl.Str("projection\x00")), 1, false, &projection[0])
+		gl.Uniform1f(gl.GetUniformLocation(programId, gl.Str("angle\x00")), float32(glfw.GetTime()))
+
 		// 1st attribute buffer : vertices
 		gl.EnableVertexAttribArray(0)
 		gl.BindBuffer(gl.ARRAY_BUFFER, vertexbuffer)
-
-		gl.UniformMatrix4fv(mvpId, 1, false, &mvp[0])
-
 		gl.VertexAttribPointerWithOffset(0, 3, gl.FLOAT, false, 0, 0)
-		gl.DrawArrays(gl.TRIANGLES, 0, 3) // Starting from vertex 0; 3 vertices total -> 1 triangle
 
+		// 2nd attribute buffer : colors
+		gl.EnableVertexAttribArray(1)
+		gl.BindBuffer(gl.ARRAY_BUFFER, colorbuffer)
+		gl.VertexAttribPointerWithOffset(1, 3, gl.FLOAT, false, 0, 0)
+
+		// do the thing!!
+		gl.DrawArrays(gl.TRIANGLES, 0, 12*3)
 		gl.DisableVertexAttribArray(0)
+		gl.DisableVertexAttribArray(1)
 
 		// Update window
 		window.SwapBuffers()
--- shaders/shader_vert.glsl
+++ shaders/shader_vert.glsl
@@ -1,9 +1,23 @@
 #version 330 core
 layout (location = 0) in vec3 vertexPosition_modelspace;
+layout (location = 1) in vec3 vertexColor;
+out vec3 fragmentColor;
 
-uniform mat4 MVP;
+uniform mat4 model; // Model matrix
+uniform mat4 view; // View matrix
+uniform mat4 projection; // Projection matrix
+uniform float angle; // Rotation angle
+
 
 void main()
 {
-    gl_Position = MVP * vec4(vertexPosition_modelspace, 1);
+    fragmentColor = vertexColor;
+  
+    mat4 rotation = mat4(
+        cos(angle), -sin(angle), 0, 0,
+        sin(angle), cos(angle), 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1
+    );
+    gl_Position = projection * view * model * rotation * vec4(vertexPosition_modelspace, 1.0);
 }
